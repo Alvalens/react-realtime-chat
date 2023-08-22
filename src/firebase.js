3ff57023840839c129df2b6d59dbb1a399e2612c
Alvalen <alvalen.shafel04@gmail.com>
2023-08-22 11:55:24 +0700

    [chore] setup env

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,21 @@
+/* eslint-disable no-undef */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
+
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyDFfiM0ojFWAgIRrTHCUictaDWDDuEKF64",
-	authDomain: "react-chat-11404.firebaseapp.com",
-	projectId: "react-chat-11404",
-	storageBucket: "react-chat-11404.appspot.com",
-	messagingSenderId: "420904262996",
-	appId: "1:420904262996:web:288f2cb5f48a73cb21fcc1",
-	measurementId: "G-1V628VLT6M",
+	apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+	authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_REACT_APP_APP_ID,
+	measurementId: import.meta.env.VITE_REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
