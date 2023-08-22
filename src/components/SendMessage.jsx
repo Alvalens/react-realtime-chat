71d0a899235f6acca0b5b985d717d1a719e0d877
Alvalen <alvalen.shafel04@gmail.com>
2023-08-22 12:02:28 +0700

    [feat] setup proptypes

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -56,7 +56,7 @@ const SendMessage = (scroll) => {
 					) : (
 										<button
 					type="submit"
-					className="text-white bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 min-w-[10%]">
+					className="text-white bg-gray-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ml-3">
 					Send
 					<FontAwesomeIcon icon={faPaperPlane} className="ml-2" />
 				</button>
