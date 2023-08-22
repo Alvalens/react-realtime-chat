9102d88cf8dc636e68ddc4d686d859a6ebce0db7
Alvalen <alvalen.shafel04@gmail.com>
2023-08-22 12:02:28 +0700

    [feat] setup proptypes

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,17 +1,18 @@
+import PropTypes from "prop-types";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import {ChatBubble} from "react-daisyui";
+import { ChatBubble } from "react-daisyui";
 
 const Message = ({ message }) => {
-  const { text, uid, avatar, name, createdAt } = message;
+	const { text, uid, avatar, name, createdAt } = message;
 
-  const [user] = useAuthState(auth);
-  const messageClass = uid === user.uid ? "sent" : "received";
+	const [user] = useAuthState(auth);
+	const messageClass = uid === user.uid ? "sent" : "received";
 	const time = new Date(createdAt?.toDate()).toLocaleTimeString([], {
 		hour: "2-digit",
 		minute: "2-digit",
 	});
-  return (
+	return (
 		<>
 			{messageClass === "received" ? (
 				<ChatBubble>
@@ -33,8 +34,19 @@ const Message = ({ message }) => {
 				</ChatBubble>
 			)}
 		</>
-  );
+	);
 };
 
+Message.propTypes = {
+	message: PropTypes.shape({
+		text: PropTypes.string.isRequired,
+		uid: PropTypes.string.isRequired,
+		avatar: PropTypes.string.isRequired,
+		name: PropTypes.string.isRequired,
+		createdAt: PropTypes.shape({
+			toDate: PropTypes.func.isRequired,
+		}).isRequired,
+	}).isRequired,
+};
 
 export default Message;
